287ac49d1fce730bf008eceb3fc49d62a40d4dce
Langsrud <oyl@ssb.no>
2023-01-31 13:58:56 +0100

    Intern funksjon som må med fra Kostra Fra fila CheckInput.r

--- R/DiverseFunksjoner.R
+++ R/DiverseFunksjoner.R
@@ -94,7 +94,13 @@ CharacterReCode = function(x,oldLevels, newLevels){
 
 
 
-
+# HeadEnd(1:1000) '1' '2' '3' '4' '...'  '1000'
+HeadEnd <- function(x, n = 4L) {
+  x <- as.character(x)
+  if (length(x) > (n + 2))
+    x <- c(head(x, n = n), "...", tail(x, n = 1))
+  x
+}
 
 
 
