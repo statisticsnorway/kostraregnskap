ccdf1097179d5eeb4fa1514c9f80d78c9f7abb37
Langsrud <oyl@ssb.no>
2023-02-08 15:45:50 +0100

    Flere uuid_generate varianter

--- R/uuid_generate.R
+++ R/uuid_generate.R
@@ -1,17 +1,33 @@
 
-#' `UUIDgenerate` med `use.time = TRUE`
+#' `UUIDgenerate` with `use.time` specified
 #' 
-#' \code{\link{UUIDgenerate}} fra pakken `uuid` med `use.time = TRUE`
+#' \code{\link{UUIDgenerate}} from package `uuid` with `use.time` specified 
+#' * **`uuid_generate`**  uses default `use.time`
+#' * **`uuid_generate_time`**  uses `use.time = TRUE`
+#' * **`uuid_generate_random`**  uses `use.time = FALSE`
 #' 
 #' @param n number of UUIDs to generate
 #'
-#' @return id-er
+#' @return character vector
 #' @export
 #' @importFrom uuid UUIDgenerate
 #'
 #' @examples
+#' uuid_generate_time(1)
 #' uuid_generate_time(2)
+#' uuid_generate_random(3)
+uuid_generate = function(n){
+  UUIDgenerate(n = n)
+}
+
+#' @rdname uuid_generate
+#' @export
 uuid_generate_time = function(n){
   UUIDgenerate(use.time = TRUE, n = n)
 }
 
+#' @rdname uuid_generate
+#' @export
+uuid_generate_random = function(n){
+  UUIDgenerate(use.time = FALSE, n = n)
+}
